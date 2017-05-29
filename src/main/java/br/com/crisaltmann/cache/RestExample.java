f5baea81377a637a3de5ca99cb1f58fdf5a225a5
crisaltmann <crisaltmann@gmail.com>
2017-05-29 00:29:10 -0300

    ajustes cache.

--- src/main/java/br/com/crisaltmann/cache/RestExample.java
+++ src/main/java/br/com/crisaltmann/cache/RestExample.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,20 @@ public class RestExample {
 	@GetMapping(value = "/{param}")
 	public ResponseEntity<String> getTexto(@PathVariable("param") String param) {
 		return ResponseEntity.ok("r: " + comp.getTextoCache(param));
+	}
+	
+	@GetMapping(value = "/remove")
+	public ResponseEntity<String> removeCache(@RequestParam(value = "key", required = false) String vl) {
+		if (vl != null) {
+			comp.remove(vl);
+		}
+		return ResponseEntity.ok("r: " + comp.getTextoCache(vl));
+	}
+	
+	@GetMapping(value = "/removeAll")
+	public ResponseEntity<String> removeCacheAll() {
+		comp.removeAll();
+		return ResponseEntity.ok("remove all");
 	}
 	
 }
--- src/main/java/br/com/crisaltmann/cache/TextoComponent.java
+++ src/main/java/br/com/crisaltmann/cache/TextoComponent.java
@@ -2,20 +2,32 @@ package br.com.crisaltmann.cache;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
+@CacheConfig(cacheNames = "general")
 public class TextoComponent {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RestExample.class);
 	
 	private Integer cont = 0;
 	
-	@Cacheable(cacheNames = "general", key = "#texto")
+	@Cacheable(key = "#texto")
 	public String getTextoCache(String texto) {
 		LOGGER.error("metodo");
 		return cont++ + texto;
 	}
 	
+	@CacheEvict
+	public void remove(String texto) {
+		LOGGER.error("remove");
+	}
+	
+	@CacheEvict(allEntries = true)
+	public void removeAll() {
+		LOGGER.error("remove");
+	}
 }
